9a6fcd1cfbe51b83f178e3064702c9afc251a955
Max Kobozev <quesesr@gmail.com>
2020-08-15 17:15:48 +0500

    Added example POST-route to hello.controller.ts

--- src/main/web/hello.controller.ts
+++ src/main/web/hello.controller.ts
@@ -1,9 +1,21 @@
-import { Controller, Get } from '@nestjs/common'
+import { Body, Controller, Get, Post } from '@nestjs/common'
+import { TObjectLiteral } from '../../lib/core/lang/lyxe-lang'
+import { DomainEntityValidationError } from '../../lib/core/domain-errors/DomainEntityValidationError'
+import { EntityValidationErrorInfo } from '../../lib/persistence/EntityValidationErrorInfo'
 
-@Controller('hello')
+@Controller('/api/hello')
 export class HelloController {
   @Get('')
   public hello () {
     return { message: 'Hello from LyxeJS!' }
+  }
+
+  @Post('')
+  public namedHello (@Body() body: TObjectLiteral) {
+    if (!body || !body.name) {
+      throw new DomainEntityValidationError(new EntityValidationErrorInfo('name', 'name is required'))
+    }
+
+    return { message: body.name + ', hello from LyxeJS!' }
   }
 }
