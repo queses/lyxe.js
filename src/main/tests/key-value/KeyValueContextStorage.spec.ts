237c90c629f5205a70ef4e79ea26449be30e7192
Max Kobozev <quesesr@gmail.com>
2020-08-18 12:07:32 +0500

    Addet zip storing test in KeyValueContextStorage.spec.ts

--- src/main/tests/key-value/KeyValueContextStorage.spec.ts
+++ src/main/tests/key-value/KeyValueContextStorage.spec.ts
@@ -3,6 +3,11 @@ import { KeyValueContextStorageTkn } from '../../../lib/key-value/lyxe-key-value
 import { itInTransaction } from '../../../lib/testing/mocha/it-in-transaction'
 import { TransactionalUseCase } from '../../../lib/persistence/annotations/TransactionalUseCase'
 import { BaseUseCase } from '../../../lib/core/context/BaseUseCase'
+import { gzip, unzip } from 'zlib'
+import { promisify } from 'util'
+
+const gzipPromise = promisify(gzip)
+const unzipPromise = promisify(unzip)
 
 describe('KeyValueContextStorage', function () {
   itInTransaction('should save and get item', async function (sf) {
@@ -43,6 +48,28 @@ describe('KeyValueContextStorage', function () {
 
     const storage = sf.createContextService(KeyValueContextStorageTkn)
     assert.isFalse(await storage.has(key))
+  }, 'test')
+
+
+  itInTransaction('should store zipped text', async function (sf) {
+    const sourceStr = 'latin latin latin кириллица кириллица кириллица ²√123 ²√123 ²√123'
+    const zipped = await gzipPromise(Buffer.from(sourceStr)) as Buffer
+    const unzipped = await unzipPromise(zipped) as Buffer
+    assert.equal(sourceStr.length, unzipped.toString().length)
+
+    const zippedStr = zipped.toString('latin1')
+    assert.isBelow(zippedStr.length, sourceStr.length)
+
+    const storage = sf.createContextService(KeyValueContextStorageTkn)
+    const key = 'key-value-storage-zip-test'
+    await storage.set(key, zippedStr)
+
+    assert.isTrue(await storage.has(key))
+    const zippedStoredStr = await storage.get(key)
+    assert.equal(zippedStr, zippedStoredStr)
+    const unzippedStored = await unzipPromise(Buffer.from(zippedStoredStr, 'latin1')) as Buffer
+
+    assert.equal(unzippedStored.toString().length, sourceStr.length)
   }, 'test')
 })
 
