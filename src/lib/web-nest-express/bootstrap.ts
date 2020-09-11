29128c10befb7aa56e3879f6f0aa17ec468444a2
Max Kobozev <quesesr@gmail.com>
2020-09-11 14:07:47 +0500

    Added NestRequestLogger

--- src/lib/web-nest-express/bootstrap.ts
+++ src/lib/web-nest-express/bootstrap.ts
@@ -2,5 +2,5 @@ import { LyxeFramework } from '../core/LyxeFramework'
 
 export default () => {
   LyxeFramework.requirePlugins('web-express')
-  require('./NestLogger')
+  require('./WinstonNestLogger')
 }
